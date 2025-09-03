9f7c0598af656bd6908db7c52c1fc4a1332e9382
atulastey-drreddys <atul.astey@drreddysfoundation.org>
2025-09-03 09:03:26 +0000

    Sep 3, 2025, 2:33 PM

--- Machine learning/NLP Gen AI/PDFChatConvervationUI.py
+++ Machine learning/NLP Gen AI/PDFChatConvervationUI.py
@@ -3,4 +3,5 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_chroma import Chroma
-from langchain_google_genai import ChatGoogle
+from langchain_google_genai import ChatGoogleGenerativeAI
+
