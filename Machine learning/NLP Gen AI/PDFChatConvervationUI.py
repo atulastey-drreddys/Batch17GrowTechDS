d0d294d2069e4dc561b5073c1e1d1c76415f3956
atulastey-drreddys <atul.astey@drreddysfoundation.org>
2025-09-03 09:04:55 +0000

    Sep 3, 2025, 2:34 PM

--- Machine learning/NLP Gen AI/PDFChatConvervationUI.py
+++ Machine learning/NLP Gen AI/PDFChatConvervationUI.py
@@ -6,5 +6,5 @@ from langchain_chroma import Chroma
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_core.prompts import Chat
+from langchain_core.prompts import ChatPromptTemplate
 
