51d944d0f104e224dca66492be99a0d84c838662
atulastey-drreddys <atul.astey@drreddysfoundation.org>
2025-09-03 09:44:17 +0000

    Sep 3, 2025, 3:14 PM

--- Machine learning/NLP Gen AI/PDFChatConvervationUI.py
+++ Machine learning/NLP Gen AI/PDFChatConvervationUI.py
@@ -32,7 +32,7 @@ retriever = vectorstore.as_retriever(search_type = 'similarity')
 
 # define
 
-llm = ChatGoogleGenerativeAI(model = 'gemini-1.5-f')
+llm = ChatGoogleGenerativeAI(model = 'gemini-1.5-flash')
 
 query = st.chat_input("Ask me anything: ")
 promt = query
