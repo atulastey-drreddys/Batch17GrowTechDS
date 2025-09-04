65e8a9e912637bee3557b86a0188b74dac7b864d
atulastey-drreddys <atul.astey@drreddysfoundation.org>
2025-09-04 05:21:37 +0000

    Sep 4, 2025, 10:51 AM

--- Machine learning/NLP Gen AI/WebsiteSummerizer.py
+++ Machine learning/NLP Gen AI/WebsiteSummerizer.py
@@ -14,4 +14,4 @@ def scrape_website_content(url):
         return e
     
 def Summerize_with_gemini(content):
-    promt = f"Summerize the following website content in a very simple point"
+    promt = f"Summerize the following website content in a very simple point {con}"
