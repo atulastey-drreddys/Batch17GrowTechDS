5b99e8616e9972158f3478179c1331cc68a0f250
atulastey-drreddys <atul.astey@drreddysfoundation.org>
2025-09-04 07:13:35 +0000

    Sep 4, 2025, 12:43 PM

--- Machine learning/NLP Gen AI/WebsiteSummerizer.py
+++ Machine learning/NLP Gen AI/WebsiteSummerizer.py
@@ -1,7 +1,7 @@
 import google.generativeai as genai
 from newspaper import Article
 
-genai.configure(api_key = "AIzaSyBHtrj9Py_7uS8o8tLBYOsh3Qw9I5NPWkA")
+genai.configure(api_key = "")
 model = genai.GenerativeModel("gemini-2.5-flash")
 
 def scrape_website_content(url):
