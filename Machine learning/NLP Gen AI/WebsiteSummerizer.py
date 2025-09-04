6ba0edf34ef246e3f3530cb78e26b8745465490f
atulastey-drreddys <atul.astey@drreddysfoundation.org>
2025-09-04 05:16:29 +0000

    Sep 4, 2025, 10:46 AM

--- Machine learning/NLP Gen AI/WebsiteSummerizer.py
+++ Machine learning/NLP Gen AI/WebsiteSummerizer.py
@@ -2,5 +2,7 @@ import google.generativeai as genai
 from newspaper import Article
 
 genai.configure(api_key = "AIzaSyBHtrj9Py_7uS8o8tLBYOsh3Qw9I5NPWkA")
-model = genai.GenerativeModel()
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+
 
